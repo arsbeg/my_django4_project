5fbaec7c3f119145a482c62559b5b45d04a97f48
arsbeg <arsbeg@mail.ru>
2024-02-06 10:48:45 +0400

    added image im models

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     prise = models.IntegerField()
     description = models.CharField(max_length=2000)
+    images = models.ImageField(blank=True, upload_to="images")
 
     def __str__(self):
         return self.name
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     # http://127.0.0.1:8000/hello/
     path('myapp/', include('myapp.urls', namespace="myapp"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
